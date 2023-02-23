5208cab157a6ed7e2941f450dfb3c47dcc215ba2
dr3jc <drejc.xt@gmail.com>
2023-02-23 19:07:27 +0100

    Končna verzija programa

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Gravitacija {
+
+    public static void main(String[] args) {
+        System.out.println("OIS je zakon!");
+        Scanner sc = new Scanner(System.in);
+        double visina = sc.nextInt();
+        izpis(visina);
+    }
+
+    public static double izracunGravitacije(double visina) {
+        double C = 6.673e-11;
+        double M = 5.972e24;
+        double r = 6.371e6;
+        return (C * M) / Math.pow(r + visina, 2.0);
+    }
+
     public static void izpis(double visina) {
-        System.out.prinf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n",
+        System.out.printf("Gravitacijski pospesek na %.2f km nadmorske visine je %.2f m/s^2\n",
 		visina, izracunGravitacije(visina * 1000));
     }
+}
