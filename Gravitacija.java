6916bc5497390b785f07ed741eff2d8f1e3c44b0
biftekk <klemen.kavcic4@gmail.com>
2023-02-23 18:32:42 +0100

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,9 +1,10 @@
 public class Gravitacija {
     
-    public static void izpis(double visina) {
-        System.out.prinf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n",
-		visina, izracunGravitacije(visina * 1000));
+    public static double izracunGravitacije(double visina) {
+		double C = 6.674e-11;
+		double M = 5.972e24;
+		double r = 6.371e6;
+		return (C * M) / Math.pow(r + visina, 2.0);
+        
     }
-	
-
 }
